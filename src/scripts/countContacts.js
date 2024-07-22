03ab6d5fc37c9261dd5f7fa0b184aea3e98635c5
EspFerevo <olekchekrenov@gmail.com>
2024-07-22 20:14:48 +0300

    Edit

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,18 +1,37 @@
 import { PATH_DB } from '../constants/contacts.js';
-import fs from 'fs/promises';
+import fs from "node:fs/promises";
 
 export const countContacts = async () => {
-  try {
-    const data = await fs.readFile(PATH_DB, 'utf-8');
-    const parseData = JSON.parse(data);
-    const totalContacts = parseData.length;
-    return totalContacts;
-  } catch (error) {
-    console.error('Error reading or parsing the file:', error);
-    return 0;
-  }
+	try {
+		const data = await fs.readFile(PATH_DB, "utf-8");
+		const contacts = JSON.parse(data);
+
+		return contacts.length;
+
+	} catch (error) {
+		console.log(error);
+	}
 };
 
-(async () => {
-  console.log(await countContacts());
-});
+console.log(await countContacts());
+
+// import { PATH_DB } from '../constants/contacts.js';
+// import fs from 'fs/promises';
+
+// export const countContacts = async () => {
+//   try {
+//     const data = await fs.readFile(PATH_DB, 'utf-8');
+//     const parseData = JSON.parse(data);
+//     const totalContacts = parseData.length;
+//     return totalContacts;
+//   } catch (error) {
+//     console.error('Error reading or parsing the file:', error);
+//     return 0;
+//   }
+// };
+
+// (async () => {
+//   console.log(await countContacts());
+// });
+
+
